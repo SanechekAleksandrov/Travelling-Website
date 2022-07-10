8a8c052131cf12f46e177f34d7319bf2a7ed61b3
SanechekAleksandrov <101074817+SanechekAleksandrov@users.noreply.github.com>
2022-07-11 02:23:39 +0300

    added slider

--- app/js/main.js
+++ app/js/main.js
@@ -2,12 +2,14 @@ $(function () {
 
 
 
-   $('.header-slider').slick({
+   $('.header-slider__inner').slick({
       slidesToShow: 1,
       slidesToScroll:1,
       autoplay:true,
       autoplaySpeed: 3000,
       dots:true, 
+      arrows: false,
+      fade: true,
     });
   
 
