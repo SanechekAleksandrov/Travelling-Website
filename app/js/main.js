f50c2e3b5f237d7e214debfdb6623b346fcb5f89
SanechekAleksandrov <101074817+SanechekAleksandrov@users.noreply.github.com>
2022-07-15 01:11:05 +0300

    added reviews slider

--- app/js/main.js
+++ app/js/main.js
@@ -11,6 +11,19 @@ $(function () {
       arrows: false,
       fade: true,
     });
+
+    $('.reviews__slider').slick({
+      slidesToShow: 1,
+      slidesToScroll:1,
+      autoplay:true,
+      autoplaySpeed: 3000,
+      dots:true, 
+      fade: true,
+      prevArrow:'<button type="button" class="slick-prev"><img src="images/reviews/arrow-left.svg" alt="arrow"></button>',
+      nextArrow:'<button type="button" class="slick-next"><img src="images/reviews/arrow-right.svg" alt="arrow"></button>'
+    });
+
+
   
 
 
