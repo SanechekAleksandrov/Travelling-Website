ca4f7a8258111f228a564f51a8ec859b1062832f
SanechekAleksandrov <101074817+SanechekAleksandrov@users.noreply.github.com>
2022-07-17 01:21:20 +0300

    added scroll

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,5 @@
 $(function () {
 
-
-
    $('.header-slider__inner').slick({
       slidesToShow: 1,
       slidesToScroll:1,
@@ -22,6 +20,13 @@ $(function () {
       prevArrow:'<button type="button" class="slick-prev"><img src="images/reviews/arrow-left.svg" alt="arrow"></button>',
       nextArrow:'<button type="button" class="slick-next"><img src="images/reviews/arrow-right.svg" alt="arrow"></button>'
     });
+
+    $(".header-nav__menu a, .footer__rights-btn").on("click", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+        top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1000);
+      });
 
 
   
